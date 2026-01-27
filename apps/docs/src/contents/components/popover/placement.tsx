6503662d3171244123d7912d8f2f4a9d16817674
kjpmj <kjpmj@tnctec.co.kr>
2026-01-28 00:27:24 +0900

    Popover 컴포넌트 추가 중

---
+++ apps/docs/src/contents/components/popover/placement.tsx
@@ -0,0 +1,33 @@
+import { Frame } from '@repo/ui/components/frame'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@repo/ui/components/popover'
+
+const sides = ['top', 'bottom', 'left', 'right'] as const
+const aligns = ['start', 'center', 'end'] as const
+
+export default function Placement() {
+  return (
+    <div className="gap-sw-xl flex">
+      {aligns.map((align) => (
+        <div key={align} className="gap-sw-xl flex flex-col">
+          {sides.map((side) => (
+            <Popover key={side}>
+              <PopoverTrigger>{`${side}-${align}`}</PopoverTrigger>
+              <PopoverContent side={side} align={align}>
+                <Frame>
+                  Content {side}-{align}
+                </Frame>
+                <Frame>
+                  Content {side}-{align}
+                </Frame>
+              </PopoverContent>
+            </Popover>
+          ))}
+        </div>
+      ))}
+    </div>
+  )
+}
