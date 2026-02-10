00c14de0c7d30969fb35d0b10d2fcd6fc79c2398
kjpmj <kjpmj@tnctec.co.kr>
2026-02-10 23:06:30 +0900

    form combobox 추가

---
+++ apps/docs/src/contents/components/form/with-combobox.tsx
@@ -0,0 +1,67 @@
+'use client'
+
+import { Button } from '@repo/ui/components/button'
+import { Combobox } from '@repo/ui/components/combobox'
+import { Form } from '@repo/ui/components/form'
+import { useState } from 'react'
+
+const items = [
+  { label: '사과', value: 'apple' },
+  { label: '바나나', value: 'banana' },
+  { label: '오렌지', value: 'orange' },
+]
+
+export default function WithCombobox() {
+  const [errors, setErrors] = useState({})
+  const [loading, setLoading] = useState(false)
+
+  return (
+    <Form
+      className="gap-sw-md flex flex-col"
+      errors={errors}
+      onSubmit={async (event) => {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const value = formData.get('fruit') as string
+
+        setLoading(true)
+        const response = await submitForm(value)
+        const serverErrors = {
+          fruit: response.error,
+        }
+
+        setErrors(serverErrors)
+        setLoading(false)
+      }}
+    >
+      <Combobox
+        name="fruit"
+        label="Fruit"
+        defaultValue="banana"
+        items={items}
+      />
+
+      <div className="flex justify-end">
+        <Button type="submit" isLoading={loading}>
+          Submit
+        </Button>
+      </div>
+    </Form>
+  )
+}
+
+async function submitForm(value: string) {
+  await new Promise((resolve) => {
+    setTimeout(resolve, 500)
+  })
+
+  try {
+    if (value === 'banana') {
+      return { error: 'Banana is not allowed' }
+    }
+  } catch {
+    return { error: 'This is not a valid fruit' }
+  }
+
+  return { success: true }
+}
